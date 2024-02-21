c15f5748614a71dc176dcebfd58f9d4afc86f6c0
Roth, Yannik <yannik.roth@juliusbaer.com>
2024-02-21 08:47:05 -0600

    intermediary commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,70 @@
+import reactImg from './assets/react-core-concepts.png';
+import { CORE_CONCEPTS } from './data.js';
+
+const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
+function Header() {
+  const description = reactDescriptions.at(getRandomInt(2));
+
+  return (
+    <header>
+    <img src={reactImg} alt="Stylized atom" />
+    <h1>React Essentials</h1>
+    <p>
+      {description} React concepts you will need for almost any app you are
+      going to build!
+    </p>
+    <p>But you can also call a function here, random int: {getRandomInt(100)}</p>
+  </header>
+  );
+}
+
+/* only 1 parameter allowed, should be called props */
+/*
+function CoreConcept(props) {
+  return (
+    <li>
+      <img src={props.image} alt={props.alt} />
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+    </li>
+  );
+}
+*/
+
+/* here we are using destructuring */
+function CoreConcept({image, title, description}) {
+  return (
+    <li>
+      <img src={image} alt={title} />
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </li>
+  );
+}
+
 function App() {
   return (
     <div>
-      <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
+      <Header></Header>
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+        <h2>Core concepts</h2>
+        <ul>
+          <CoreConcept 
+            title={CORE_CONCEPTS[0].title}
+            description={CORE_CONCEPTS[0].description}
+            image={CORE_CONCEPTS[0].image}
+          />
+          <CoreConcept {...CORE_CONCEPTS[1]}/>
+          <CoreConcept {...CORE_CONCEPTS[2]}/>
+          <CoreConcept {...CORE_CONCEPTS[3]}/>
+        </ul>
+        </section>
       </main>
     </div>
   );
